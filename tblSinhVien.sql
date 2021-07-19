199ab1ff0b0533b636e4761eb1f88bc845ad92ec
theshysadec98 <lyminhhoang01041998@gmail.com>
2021-07-19 09:26:00 +0700

    update

--- tblSinhVien.sql
+++ tblSinhVien.sql
@@ -27,7 +27,9 @@ create table  qlsv.tblSinhVien(
 insert into qlsv.tblkhoa value (1, "CNTT");
 insert into qlsv.tbllop value (16, "muoi mot", 1);
 insert into qlsv.tblsinhvien value (1,"Phan ", "Trung", '1994/11/11', 12, 88.5, 16, 1);
-
+insert into qlsv.tblkhoa value (3, "CN");
+insert into qlsv.tbllop value (17, "muoi mot nam", 2);
+insert into qlsv.tblsinhvien value (2,"Tuan ", "Phan", '1993/11/11', 12, 88.5, 16, 2);
 -- c1 ---
 select sv_Maso, sv_Hodem, sv_Ten, sv_Ngaysinh, sv_Lop, sv_IDKhoa, l_Ten, k_Ten
 from qlsv.tblSinhVien 
@@ -50,5 +52,5 @@ select tblSinhVien.sv_Maso, tblSinhVien.sv_Hodem, tblSinhVien.sv_Ten, tblSinhVie
 from qlsv.tblSinhVien 
 inner join qlsv.tblLop on tblSinhVien.sv_IDLop = tbllop.l_ID 
 inner join qlsv.tblKhoa on tblSinhVien.sv_IDLop = tblKhoa.k_ID
-where l_Khoa  like "%CNTT%";
+where l_Khoa  = "CNTT";
 
