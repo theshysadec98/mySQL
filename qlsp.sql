c6a17b12560d1e87e5f4474970c2db1e317be7aa
theshysadec98 <lyminhhoang01041998@gmail.com>
2021-07-20 09:38:31 +0700

    update

---
+++ qlsp.sql
@@ -0,0 +1,100 @@
+create database qlsp;
+create table custumer(
+	hoten nvarchar(50) not null,
+    id int not null primary key,
+    diachi nvarchar(100) not null
+);
+
+create table sanpham(
+	tensp nvarchar(50) not null,
+    id int not null primary key primary key,
+    gia double not null,
+    xuatxu nvarchar(50) not null
+);
+
+create table hoadon(
+	id int not null primary key,
+    ngay date not null,
+    tonggia double not null,
+	id_kh int not null,
+    foreign key (id_kh) references custumer(id)
+    
+);
+
+create table hoadonct(
+	id_sp int not null,
+    id_hd int not null,
+	soluong_hoadon int not null, 
+    foreign key (id_sp) references sanpham(id),
+    foreign key (id_hd) references hoadon(id)
+);
+
+
+-- c6
+select soluong_hoadon, tonggia
+from hoadon 
+inner join hoadonct on  hoadon.id = hoadonct.id_hd
+where ngay between '2006-06-19' and '2006-06-21';
+
+-- c7 
+select soluong_hoadon, tonggia, ngay
+from hoadon 
+inner join hoadonct on  hoadon.id = hoadonct.id_hd
+where ngay between '2006-06-01' and '2006-07-01'
+order by tonggia desc, ngay asc;
+
+-- c8
+select custumer.id, custumer.hoten
+from custumer
+inner join hoadon on custumer.id = hoadon.id_kh
+where ngay = '2007-06-19';
+
+-- c10
+select sanpham.id, sanpham.tensp
+from qlsv.sanpham
+inner join hoadonct on sanpham.id = hoadonct.id_sp
+inner join hoadon on hoadonct.id_hd = hoadon.id
+inner join custumer on hoadon.id_kh = custumer.id
+where (custumer.hoten like '%Nguyen Van A%') and (ngay between '2006-10-01' and '2006-11-01');
+
+
+-- c11
+select hoadon.ngay, hoadon.id
+from hoadon
+inner join hoadonct on hoadon.id = hoadonct.id_hd
+inner join sanpham on hoadonct.id_sp = sanpham.id
+where tensp like '%Máy giặt%' or '%Tủ lạnh%';
+
+-- c12 Tìm các số hóa đơn đã mua sản phẩm “Máy giặt” hoặc “Tủ lạnh”, mỗi sản phẩm muavới số lượng từ 10 đến 20
+select hoadon.id
+from hoadon
+inner join hoadonct on hoadon.id = hoadonct.id_hd
+inner join sanpham on hoadonct.id_sp = sanpham.id
+where tensp like '%Máy giặt%' or '%Tủ lạnh%'
+having soluong_hoadon between 10 and 20;
+
+-- c13 Tìm các số hóa đơn mua cùng lúc 2 sản phẩm “Máy giặt” và “Tủ lạnh”, mỗi sản phẩmmua với số lượng từ 10 đến 20
+select hoadon.id
+from hoadon
+inner join hoadonct on hoadon.id = hoadonct.id_hd
+inner join sanpham on hoadonct.id_sp = sanpham.id
+where tensp like '%Máy giặt%' and '%Tủ lạnh%'
+having soluong_hoadon between 10 and 20;
+
+-- c15  In ra danh sách các sản phẩm (MASP,TENSP) không bán được
+select sanpham.id, sanpham.tensp
+from qlsv.sanpham
+inner join hoadonct on sanpham.id = hoadonct.id_sp
+inner join hoadon on hoadonct.id_hd = hoadon.id
+inner join custumer on hoadon.id_kh = custumer.id
+where soluong_hoadon = 0;
+
+-- c16 In ra danh sách các sản phẩm (MASP,TENSP) không bán được trong năm 2006
+select sanpham.id, sanpham.tensp
+from qlsv.sanpham
+inner join hoadonct on sanpham.id = hoadonct.id_sp
+inner join hoadon on hoadonct.id_hd = hoadon.id
+inner join custumer on hoadon.id_kh = custumer.id
+where soluong_hoadon = 0
+having ngay between '2006-01-01' and '2007-01-01';
+
